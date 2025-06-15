a3d22fb2b974393d1f349b3ff671d7d9e689ddd8
Nolan MacFarlane <nolan.firestar@gmail.com>
2025-06-15 13:29:12 -0700

    Completed general page structure, titlebar and font setup.

--- script.js
+++ script.js
@@ -6,18 +6,26 @@
 
 // #region Global Variables
 
-
+    const clock = document.getElementById("clock");
 
 // #endregion
 
 // #region Main
 
-
+    updateClock();
 
 // #endregion
 
 // #region Functions
 
+    function updateClock()
+    {
+        let time = new Date();
 
+        clock.textContent = time.toLocaleTimeString();
+        
+        setTimeout(updateClock, 1000);
+    }
+    
 
 // #endregion
